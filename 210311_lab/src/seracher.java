8356c8ea0f26ddbb6d9d8f55a6bdb4677ddab057
eyu08479 <eyu08479@gmail.com>
2021-04-11 17:53:46 +0900

    CalcSim:5주차 수정내용이 아닌 6주차 추가내용입니다

--- 210311_lab/src/seracher.java
+++ 210311_lab/src/seracher.java
@@ -27,6 +27,24 @@ import org.xml.sax.SAXException;
 
 public class seracher {
 	
+	public static void CalcSim(double[] a_r,Iterator<String> its,HashMap hm,ArrayList<String> w) {
+		while(its.hasNext()) {
+			String key=its.next();
+			Object value=hm.get(key);
+			//System.out.println(key+"->"+value); //test
+			
+			if(w.contains(key)) {
+				//System.out.println(key+value);
+				ArrayList<String> row=(ArrayList<String>) value;
+				for(int k=0;k<row.size();k=k+2) {
+					//arr_value[Integer.parseInt(row.get(k))]=Math.round(arr_value[Integer.parseInt(row.get(k))]*100)/100.0+Math.round(Double.parseDouble(row.get(k+1))*100)/100.0;
+					a_r[Integer.parseInt(row.get(k))]=a_r[Integer.parseInt(row.get(k))]+Double.parseDouble(row.get(k+1));
+					a_r[Integer.parseInt(row.get(k))]=Math.round(a_r[Integer.parseInt(row.get(k))]*100)/100.0;
+				}
+			}
+		}
+	}
+	
 	public static void make_serach(String file,String q,String query) throws IOException, ClassNotFoundException, ParserConfigurationException, SAXException {
 
 		ArrayList<String> word =new ArrayList();
@@ -49,21 +67,7 @@ public class seracher {
 		
 		double[] arr_value= {0,0,0,0,0};
 		
-		while(it.hasNext()) {
-			String key=it.next();
-			Object value=hashMap2.get(key);
-			//System.out.println(key+"->"+value); //test
-			
-			if(word.contains(key)) {
-				//System.out.println(key+value);
-				ArrayList<String> row=(ArrayList<String>) value;
-				for(int k=0;k<row.size();k=k+2) {
-					//arr_value[Integer.parseInt(row.get(k))]=Math.round(arr_value[Integer.parseInt(row.get(k))]*100)/100.0+Math.round(Double.parseDouble(row.get(k+1))*100)/100.0;
-					arr_value[Integer.parseInt(row.get(k))]=arr_value[Integer.parseInt(row.get(k))]+Double.parseDouble(row.get(k+1));
-					arr_value[Integer.parseInt(row.get(k))]=Math.round(arr_value[Integer.parseInt(row.get(k))]*100)/100.0;
-				}
-			}
-		}
+		CalcSim(arr_value,it,hashMap2,word);
 //		for(int k=0;k<arr_value.length;k++)
 //			System.out.println(arr_value[k]);
 		
